bd260260b7a389c44ba24215465a9e416c0639fc
phuong936 <79656856+phuong936@users.noreply.github.com>
2021-03-02 09:56:40 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+
+function prompt() {
     var person = window.prompt("Please enter your name");
     if (person != null) {
       document.getElementById("viewsource").innerHTML =
       "Hello " + person + "! How are you today?";
     }
+}
 
 
 export default prompt;
